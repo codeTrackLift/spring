21b4adeeb104f61aa3d366092e080b308ef97e6f
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-07-06 20:19:24 -0400

    init commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState }from 'react'
+import { animated, config, useSpring } from 'react-spring'
+import './styles.css'
+
+
+const App = () => {
+  const [flip, set] = useState(false)
+  const props = useSpring({
+    to: { opacity: 1 },
+    from: { opacity: 0 },
+    reset: true,
+    reverse: flip,
+    delay: 200,
+    config: config.molasses,
+    onRest: () => set(!flip),
+  })
+
+  return <animated.h1 style={props}>hello</animated.h1>
+}
+
+function SVG() {
+  const [flip, set] = useState(false)
+  const { x } = useSpring({
+    reset: true,
+    reverse: flip,
+    from: { x: 0 },
+    x: 1,
+    delay: 200,
+    config: config.molasses,
+    onRest: () => set(!flip),
+  })
+  const points = [
+    22.5,35.25,8.68704657,42.5118994,11.3250859,27.1309497,0.150171867,16.2381006,15.5935233,13.9940503,22.5,0,29.4064767,13.9940503,44.8498281,16.2381006,33.6749141,27.1309497,36.3129534,42.5118994
+  ]
+
+  return (
+    <animated.svg
+      style={{ margin: 20, width: 80, height: 80 }}
+      viewBox="0 0 45 44"
+      strokeWidth="2"
+      fill="white"
+      stroke="rgb(45, 55, 71)"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeDasharray={156}
+      strokeDashoffset={x.to(x => (1 - x) * 156)}>
+      <polygon points={points} />
+    </animated.svg>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App /><SVG/>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
